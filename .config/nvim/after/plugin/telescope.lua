a4be8b81fb8c44acba84062071a42b4aac15be88
Daniel Vasek <daniel.vasek@freelo.io>
2025-12-10 22:25:05 +0100

    Adjust target directories for live grep

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,6 +2,22 @@ local builtin = require('telescope.builtin')
 
 local telescope = require('telescope')
 telescope.setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+      '-g', '!.git/',
+      '-g', '!.venv/',
+      '-g', '!.idea/',
+      '-g', '!.vscode/',
+    },
+  },
   pickers = {
     find_files = {
       hidden = true,
